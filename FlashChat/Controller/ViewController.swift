8cb00bfdd627e839b491329d5cef53ced7862ca0
Laura Garcia <laudeckeddevs@gmail.com>
2021-01-04 15:26:31 +0100

    first commit

---
+++ FlashChat/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlashChat
+//
+//  Created by Laura Garcia on 3/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
