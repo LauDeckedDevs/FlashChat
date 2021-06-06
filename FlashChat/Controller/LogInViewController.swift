5cf898c0d3d4662d65afbb328c876923369a855f
LauDecked <lgarciacano541@gmail.com>
2021-06-06 20:14:17 +0200

    Finished SV progresshud implementation

--- FlashChat/Controller/LogInViewController.swift
+++ FlashChat/Controller/LogInViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import SVProgressHUD
 
 
 class LogInViewController: UIViewController {
@@ -30,22 +31,22 @@ class LogInViewController: UIViewController {
     //MARK: - Log-inProcess
    
     @IBAction func logInPressed(_ sender: AnyObject) {
+                
+        SVProgressHUD.show()
+        self.view.endEditing(true)
+        
         Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (use, error) in
             if error != nil {
                 let alertFailed = UIAlertController(title: "Login", message: "Login failed, try again later", preferredStyle: UIAlertController.Style.alert)
+                SVProgressHUD.dismiss()
                 self.present(alertFailed, animated: true, completion: nil)
                 let when = DispatchTime.now() + 1
                 DispatchQueue.main.asyncAfter(deadline: when){
                   alertFailed.dismiss(animated: true, completion: nil)
                 }
             } else {
-                let alertSuccessful = UIAlertController(title: "Login", message: "Login sucessfully", preferredStyle: UIAlertController.Style.alert)
-                self.present(alertSuccessful, animated: true, completion: nil)
-                let when = DispatchTime.now() + 0.5
-                DispatchQueue.main.asyncAfter(deadline: when){
-                  alertSuccessful.dismiss(animated: true, completion: nil)
-                    self.performSegue(withIdentifier: "goToChat", sender: self)
-                }
+                SVProgressHUD.dismiss()
+                self.performSegue(withIdentifier: "goToChat", sender: self)
             }
         }
     }
--- FlashChat/Controller/RegisterViewController.swift
+++ FlashChat/Controller/RegisterViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import SVProgressHUD
 
 
 class RegisterViewController: UIViewController {
@@ -29,11 +30,14 @@ class RegisterViewController: UIViewController {
     //MARK: - RegistrationProcess
   
     @IBAction func registerPressed(_ sender: AnyObject) {
+        
+        SVProgressHUD.show()
+        
         Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
             if error != nil {
                 print(error!)
             } else {
-                print("Registration succesful!")
+                SVProgressHUD.dismiss()
                 self.performSegue(withIdentifier: "goToChat", sender: self)
             }
         }
