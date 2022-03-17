f7077451fd3da1f0064ebf4e092ef902a910cc79
Laura Garcia <laudeckeddevs@gmail.com>
2022-03-17 22:53:51 +0100

    GoogleSignIn Cocoapods implementation

--- FlashChat/Controller/AppDelegate.swift
+++ FlashChat/Controller/AppDelegate.swift
@@ -46,6 +46,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
 
 //MARK: - Uncomment this only once you've gotten to Step 14.
     /*
