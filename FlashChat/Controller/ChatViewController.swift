02d6a26b8f1a4b7363b4de5a0e31100b88b6b51b
Laura Garcia <laudeckeddevs@gmail.com>
2023-08-14 19:58:32 +0200

    Supression of the back button

--- FlashChat/Controller/ChatViewController.swift
+++ FlashChat/Controller/ChatViewController.swift
@@ -2,8 +2,6 @@
 //  ViewController.swift
 //  Flash Chat
 //
-//  Created by Angela Yu on 29/08/2015.
-//  Copyright (c) 2015 London App Brewery. All rights reserved.
 //
 
 import UIKit
@@ -24,6 +22,7 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     //MARK: - View
     
     override func viewDidLoad() {
+        self.navigationItem.setHidesBackButton(true, animated: true)
         super.viewDidLoad()
         messageTableView.delegate = self
         messageTableView.dataSource = self
