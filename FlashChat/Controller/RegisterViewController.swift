3bdf851d607d88b7c0f95513c8463b7ecf9989f5
Lau Garcia Cano <developments@laudreams.com>
2024-11-10 21:15:04 +0100

    improvemen

--- FlashChat/Controller/RegisterViewController.swift
+++ FlashChat/Controller/RegisterViewController.swift
@@ -34,14 +34,16 @@ class RegisterViewController: UIViewController {
         SVProgressHUD.show()
         
         Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
+        
             if error != nil {
-                let alertFailed = UIAlertController(title: "Registration failed", message: "Please try again later or with another email account", preferredStyle: UIAlertController.Style.alert)
+            let alertFailed = UIAlertController(title: "Registration failed", message: "Please try again later or with another email account", preferredStyle: UIAlertController.Style.alert)
                 SVProgressHUD.dismiss()
                 self.present(alertFailed, animated: true, completion: nil)
                 let when = DispatchTime.now() + 1
                 DispatchQueue.main.asyncAfter(deadline: when){
                   alertFailed.dismiss(animated: true, completion: nil)
                 }
+    
             } else {
                 SVProgressHUD.dismiss()
                 self.performSegue(withIdentifier: "goToChat", sender: self)
